3fc0e78d2c16cdcd05045620f91c89fb3d4ff0a6
Solace <545058656@qq.com>
2019-12-06 19:42:18 +0800

    1

---
+++ spring_boot_netty/src/main/java/com/solace/spring_boot_netty/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.solace.spring_boot_netty.controller;
+
+import cn.hutool.core.util.RandomUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/index")
+	public ModelAndView  index(){
+		ModelAndView mav=new ModelAndView("socket");
+		mav.addObject("uid", RandomUtil.randomNumbers(6));
+		return mav;
+	}
+    @RequestMapping("/socket")
+    public String  socket(){
+        return "socket";
+    }
+}
---
+++ spring_boot_netty/src/test/java/com/solace/spring_boot_netty/SpringBootNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solace.spring_boot_netty;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
